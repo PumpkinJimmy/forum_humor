316626ec5ea307ba8cadcea4d9e179ee6e5936e7
bbpumpkin <996344439@qq.com>
2021-12-23 22:21:21 +0800

    feat: SQL data import & grant & table fix

---
+++ backend/database/SQL_DCL/grant.sql
@@ -0,0 +1,3 @@
+grant select, delete, update, insert on message to forum_user;
+grant select, delete, update, insert on post to forum_user;
+grant select, delete, update, insert on forum_user to forum_user;
---
+++ backend/database/data_import.sql
@@ -0,0 +1,3 @@
+copy forum_user from 'ForumUser.csv' with csv headers;
+copy post from 'Post.csv' with csv headers;
+copy message from 'Message.csv' with csv headers;
