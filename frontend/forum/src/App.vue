4bf6d402d1860f9149c546a49ecaf42bd698b9d3
bbpumpkin <996344439@qq.com>
2022-01-10 23:21:25 +0800

    refactor: split sidebar to Panel

--- frontend/forum/src/App.vue
+++ frontend/forum/src/App.vue
@@ -19,57 +19,22 @@
 
     <v-main>
       <v-row justify="center">
-        <v-col cols="8">
+        <v-col cols="10">
           <router-view></router-view>
         </v-col>
-        <v-col cols="2">
-          <v-card>
-          <v-list>
-            <v-subheader>Models</v-subheader>
-            <v-list-item-group color="primary">
-              <v-list-item
-              v-for="modelName,idx in models"
-              :key="idx"
-              @click="route_to($event, `/object/${modelName}/`)"
-              >{{modelName}}
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-          </v-card>
-        </v-col>
       </v-row>
-
-      
-      <!-- <HelloWorld/> -->
     </v-main>
   </v-app>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld';
-import axios from 'axios'
+
 export default {
   name: "App",
-
-  components: {
-    // HelloWorld,
-  },
-
   data: () => ({
     models: []
   }),
-
-  mounted(){
-    axios.get('http://127.0.0.1:5000/api/v1/model')
-      .then((response)=>{
-        this.models = response.data.models
-      })
-  },
-
   methods: {
-    route_to(ev, loc) {
-      this.$router.push(loc);
-    },
   },
 };
 </script>
--- frontend/forum/src/router.config.js
+++ frontend/forum/src/router.config.js
@@ -1,18 +1,19 @@
 
-import ObjectListEditor from './components/ObjectListEditor.vue'
+
 import PostList from './components/PostList.vue'
 import EmotionView from './components/EmotionView'
 import UserPostViewer from './components/UserPostsViewer'
 import TagViewer from './components/TagViewer'
 import Login from './components/Login'
+import Panel from './components/Panel'
 
 const routes = [
-    {path: '/object/:modelName', component: ObjectListEditor, props: true},
     {path: '/post/', component: PostList},
     {path: '/emotion/', component: EmotionView},
     {path: '/userposts/', component: UserPostViewer},
     {path: '/tags/', component: TagViewer},
-    {path: '/login/', component: Login}
+    {path: '/login/', component: Login},
+    {path: '/panel/', component: Panel}
 ]
 
 export default routes
